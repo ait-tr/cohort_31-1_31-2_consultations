82e71f9dabf2665611db50a88e86764da39eb2ff
Evgenygroup <mailfrom61@gmail.com>
2023-07-13 09:29:57 +0200

    adds consultation 12.07

---
+++ consultations/2023-07-12/code/consultation_2023_07_12/src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+
+    public static void main(String[] args) {
+       // int[][] arr;
+        int table[][]= new int[2][3];
+        table[0][0]= 1;
+
+        int[]ints1 = new int[6];
+        ints1[0] = 1;
+
+        int[]ints ={1,2,3};
+        int[]ints2 = new int[]{1,3,4,5};
+           //          0  1 2
+    //    int[][]arr ={ {67,2,3}, //0
+    //                   {3,4,5}, //1
+    //                   {8,4} };//2
+    //    System.out.println(arr[0][0]);
+
+        int[][] arr =new int[3][];
+        arr[0]  = new int[3];
+        arr[1] = new int[3];
+        arr[2] = new int[2];
+
+        arr[0][0]= 67;
+        arr[0][1]= 2;
+        arr[0][2]= 3;
+        arr[1][0]= 3;
+        arr[1][1]= 4;
+        arr[1][2]= 5;
+        arr[2][0]= 8;
+        arr[2][1]= 4;
+
+        for(int i = 0;i < arr.length;i++){//i=1
+            for(int j = 0;j<arr[i].length;j++)
+            {
+
+             //   arr[i][j]=;
+            }
+            System.out.println();
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
