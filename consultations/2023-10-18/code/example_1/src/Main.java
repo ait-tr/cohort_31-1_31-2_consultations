47a372ff063f272d0a5ba471ff69531a05d76bb9
Alisher Khamidov <a.a.khamidov@yandex.ru>
2023-10-23 16:51:47 +0200

    add consultation

---
+++ consultations/2023-10-18/code/example_1/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.UUID;
+
+public class Main {
+  public static void main(String[] args) {
+    String id = UUID.randomUUID().toString();
+    System.out.println(id); // 507d304c-bda7-4d60-a295-237f7dbcd477
+  }
+}
